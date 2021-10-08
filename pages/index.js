bd0b914f2d8f45c9c96ca0590033c672fbe5d31e
shAtud <sidalihouda.computerscience@gmail.com>
2021-10-08 14:41:33 +0100

    drawing x^2 fonction

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,42 @@ import Head from 'next/head'
 import {Line} from 'react-chartjs-2';
 
 export default function Home() {
+  const fData = [];
+  for(let i=0;i<10;i++){
+    fData.push({x:i,y:i*i})
+  }
   const data = {
-        labels:['kawkaw','himes','besbas','fsddsf','sfdfs'],
+        labels:fData.map(d=>d.x),
         datasets:[
           {
-            datasets: [{
-              label: "Test",
-              data: [{
-                 x: 0,
-                 y: 5
-              }, {
-                 x: 5,
-                 y: 10
-              }, {
-                 x: 8,
-                 y: 5
-              }, {
-                 x: 15,
-                 y: 0
-              }],
-           }]
+            label:'khra for 2020',
+            data:fData.map(d=>d.y),
+            backgroundColor:['red','blue'],
+            borderColor:['yellow'],
+            pointBackgroundColor:['orange'],
+            pointBorderColor:['black']
+
           }
         ]
-        
 
-        
   }
   const options = {
-      responsive:true,
+      title:{
+        display:true,
+        text:'Line chart',
+
+      },
+      scales:{
+        yAxes:[
+          {
+            ticks:{
+              min:0,
+              max:6,
+              stepSize:1,
+            }
+          }
+        ]
+      }
   }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
