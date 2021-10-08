96cf0f8a41d8bdfd2c75e2a170b23ce8bb7b0ded
shAtud <sidalihouda.computerscience@gmail.com>
2021-10-08 14:47:03 +0100

    drawing x^2 fonction edit

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,18 @@ import {Line} from 'react-chartjs-2';
 
 export default function Home() {
   const fData = [];
-  for(let i=0;i<10;i++){
-    fData.push({x:i,y:i*i})
+  for(let i=-10;i<=10;i++){
+    fData.push({x:i,y:Math.pow(i,2)})
   }
   const data = {
         labels:fData.map(d=>d.x),
         datasets:[
           {
-            label:'khra for 2020',
+            label:'exp(x) ',
             data:fData.map(d=>d.y),
             backgroundColor:['red','blue'],
-            borderColor:['yellow'],
-            pointBackgroundColor:['orange'],
+            borderColor:['blue'],
+            pointBackgroundColor:['red'],
             pointBorderColor:['black']
 
           }
